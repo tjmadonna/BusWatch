5542c0c5db73473e8b07daa38b0fe39e264b474d
Tyler Madonna <developer@madonnaapps.com>
2017-12-25 12:55:02 -0500

    Added dagger dependency injection

--- app/src/main/java/com/madonnaapps/buswatch/BusWatchApplication.kt
+++ app/src/main/java/com/madonnaapps/buswatch/BusWatchApplication.kt
@@ -16,12 +16,32 @@
 
 package com.madonnaapps.buswatch
 
+import android.app.Activity
 import android.app.Application
+import com.madonnaapps.buswatch.di.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
+import javax.inject.Inject
 
-class BusWatchApplication: Application() {
+internal class BusWatchApplication: Application(), HasActivityInjector {
+
+    @Inject
+    lateinit var activityDispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
 
     override fun onCreate() {
         super.onCreate()
+
+        DaggerAppComponent
+                .builder()
+                .application(this)
+                .build()
+                .inject(this)
+
+    }
+
+    override fun activityInjector(): AndroidInjector<Activity> {
+        return activityDispatchingAndroidInjector
     }
 
 }
