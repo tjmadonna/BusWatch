69cd57f1185ccbed64c46d1b515fda3a6870a361
Tyler Madonna <developer@madonnaapps.com>
2017-12-28 15:00:30 -0500

    Moved parsing logic from stops repository to splash view model

--- app/src/main/java/com/madonnaapps/buswatch/data/StopsRepository.kt
+++ app/src/main/java/com/madonnaapps/buswatch/data/StopsRepository.kt
@@ -16,103 +16,51 @@
 
 package com.madonnaapps.buswatch.data
 
-import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.MutableLiveData
 import android.content.SharedPreferences
-import android.os.AsyncTask
+import com.madonnaapps.buswatch.data.local.Stop
 import com.madonnaapps.buswatch.data.local.StopDao
-import com.madonnaapps.buswatch.data.parse.ParseResult
 import com.madonnaapps.buswatch.data.parse.StopFileParser
 import javax.inject.Inject
 
 internal class StopsRepository @Inject constructor(private val stopDao: StopDao,
-                                           private val sharedPreferences: SharedPreferences,
-                                           private val stopFileParser: StopFileParser) {
+                                                   private val sharedPreferences: SharedPreferences,
+                                                   private val stopFileParser: StopFileParser) {
 
     private companion object {
-
-        // Stop data version. Increment this integer when updating stops.json file
-        // in res/raw directory
-        private val STOP_FILE_VERSION = 1
 
         // Stop data version shared preferences key
         private val STOP_FILE_VERSION_PREF_KEY = "stops_file_version"
 
     }
 
-    /*
-     * Function that checks if database has the latest list of stops. Updates the data if it is not
-     * the latest version
-     *
-     * @return LiveData object that carries the parsing result
-     */
-    fun stopsOnLatestFileVersion(): LiveData<ParseResult> {
+    // Stop database
 
-        val liveDataResult = MutableLiveData<ParseResult>()
-
-        // Check if the current stops data version is up to date
-        val storeFileVersion = sharedPreferences.getInt(STOP_FILE_VERSION_PREF_KEY, 0)
-
-        if (storeFileVersion != STOP_FILE_VERSION) {
-
-            // Stop data is not up to date. Parse the new stop json file
-            parseStopsFromLocalJsonFileAndSave(liveDataResult)
-
-        } else {
-
-            // Stop data is up to date. Return result with no error
-            liveDataResult.value = ParseResult()
-
-        }
-
-        return liveDataResult
-
+    fun insertStops(stops: List<Stop>) : Int {
+        val indicesInserted = stopDao.insert(stops)
+        return indicesInserted.count()
     }
 
-    /*
-     * Function that parses the json data file and inserts it into the database. Posts the results
-     * to the live data input parameter
-     *
-     * @params LiveData object that carries the parsing result
-     */
-    private fun parseStopsFromLocalJsonFileAndSave(liveData: MutableLiveData<ParseResult>) {
+    fun deleteAllStops() {
+        stopDao.deleteAll()
+    }
 
-        // Execute this on another thread
-        AsyncTask.execute({
+    // Shared preferences
 
-            // Create a parse result object
-            val parseResult = ParseResult()
+    fun getStopFileVersion() : Int {
+        return sharedPreferences.getInt(StopsRepository.STOP_FILE_VERSION_PREF_KEY, 0)
+    }
 
-            // Clear the stop database table because we are doing a full update
-            stopDao.deleteAll()
+    fun updateStopFileVersion(version : Int) {
+        sharedPreferences.edit()
+                .putInt(StopsRepository.STOP_FILE_VERSION_PREF_KEY, version)
+                .apply()
+    }
 
-            // Parse the json file and convert results to stop database objects
-            val stops = stopFileParser.parse()
-                    .map { parseStop -> parseStop.toDatabaseStop() }
+    // Stop parsing from Json file
 
-            // Insert stops into the database
-            val insertedIds = stopDao.insert(stops)
-
-            if (insertedIds.count() > 0) {
-
-                // Success: Stops were inserted. Update the current stops data version
-                // We are not on the main thread.
-                sharedPreferences.edit()
-                        .putInt(STOP_FILE_VERSION_PREF_KEY, STOP_FILE_VERSION)
-                        .apply()
-
-            } else {
-
-                // Error: No stops were inserted in the database, set the error in parse result
-                parseResult.error = Exception("No stops inserted into database")
-
-            }
-
-            // Post parse result to live data. We are not on the main thread
-            liveData.postValue(parseResult)
-
-        })
-
+    fun parseStopFile() : List<Stop> {
+        return stopFileParser.parse()
+                .map { parseStop -> parseStop.toDatabaseStop() }
     }
 
 }
--- app/src/main/java/com/madonnaapps/buswatch/splash/SplashViewModel.kt
+++ app/src/main/java/com/madonnaapps/buswatch/splash/SplashViewModel.kt
@@ -17,16 +17,91 @@
 package com.madonnaapps.buswatch.splash
 
 import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import android.os.AsyncTask
 import com.madonnaapps.buswatch.data.StopsRepository
 import com.madonnaapps.buswatch.data.parse.ParseResult
 
-internal class SplashViewModel constructor(stopsRepository: StopsRepository) : ViewModel() {
+internal class SplashViewModel constructor(val stopsRepository: StopsRepository) : ViewModel() {
 
-    private val parseResult = stopsRepository.stopsOnLatestFileVersion()
+    private companion object {
 
-    fun parseResult() : LiveData<ParseResult> {
+        // Stop data version. Increment this integer when updating stops.json file
+        // in res/raw directory
+        private val STOP_FILE_VERSION = 1
+
+    }
+
+    private val parseResult = MutableLiveData<ParseResult>()
+
+    /*
+    * Function that checks if database has the latest list of stops. Updates the data if it is not
+    * the latest version
+    *
+    * @return LiveData object that carries the parsing result
+    */
+    fun parseResult(): LiveData<ParseResult> {
+
+        // Check if the current stops data version is up to date
+        val storeFileVersion = stopsRepository.getStopFileVersion()
+
+        if (storeFileVersion != SplashViewModel.STOP_FILE_VERSION) {
+
+            // Stop data is not up to date. Parse the new stop json file
+            parseStopsFromLocalJsonFileAndSave(parseResult)
+
+        } else {
+
+            // Stop data is up to date. Return result with no error
+            parseResult.value = ParseResult()
+
+        }
+
         return parseResult
+
+    }
+
+    /*
+ * Function that parses the json data file and inserts it into the database. Posts the results
+ * to the live data input parameter
+ *
+ * @params LiveData object that carries the parsing result
+ */
+    private fun parseStopsFromLocalJsonFileAndSave(liveData: MutableLiveData<ParseResult>) {
+
+        // Execute this on another thread
+        AsyncTask.execute({
+
+            // Create a parse result object
+            val parseResult = ParseResult()
+
+            // Clear the stop database table because we are doing a full update
+            stopsRepository.deleteAllStops()
+
+            // Parse the json file and convert results to stop database objects
+            val stops = stopsRepository.parseStopFile()
+
+            // Insert stops into the database
+            val insertCount = stopsRepository.insertStops(stops)
+
+            if (insertCount > 0) {
+
+                // Success: Stops were inserted. Update the current stops data version
+                stopsRepository.updateStopFileVersion(STOP_FILE_VERSION)
+
+            } else {
+
+                // Error: No stops were inserted in the database, set the error in parse result
+                parseResult.error = Exception("No stops inserted into database")
+
+            }
+
+            // Post parse result to live data. We are not on the main thread
+            liveData.postValue(parseResult)
+
+        })
+
     }
 
 }
