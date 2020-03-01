dcb8547176757c8bff651845b3677ad83649abc2
Tyler Madonna <developer@madonnaapps.com>
2020-03-01 13:23:23 -0500

    Added dagger ApplicationComponent

---
+++ app/src/main/java/com/madonnaapps/buswatch/di/component/ApplicationComponent.kt
@@ -0,0 +1,36 @@
+package com.madonnaapps.buswatch.di.component
+
+import android.app.Application
+import com.madonnaapps.buswatch.di.module.*
+import com.madonnaapps.buswatch.ui.favorites.FavoritesFragment
+import com.madonnaapps.buswatch.ui.main.MainActivity
+import com.madonnaapps.buswatch.ui.stopmap.StopMapFragment
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [
+        ApplicationModule::class,
+        ViewModelModule::class,
+        DataModule::class,
+        LocalModule::class,
+        FileModule::class
+    ]
+)
+interface ApplicationComponent {
+
+    // Factory to create instance of ApplicationComponent
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance application: Application): ApplicationComponent
+    }
+
+    fun inject(mainActivity: MainActivity)
+
+    fun inject(stopMapFragment: StopMapFragment)
+
+    fun inject(favoritesFragment: FavoritesFragment)
+
+}
