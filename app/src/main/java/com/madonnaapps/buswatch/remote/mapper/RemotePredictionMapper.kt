3da1926f88dd8334d7719a552b40fedc7f67cdb5
Tyler Madonna <developer@madonnaapps.com>
2020-03-01 17:20:22 -0500

    Handled vehicleId and direction properties in RemotePredictionMapper

--- app/src/main/java/com/madonnaapps/buswatch/remote/mapper/RemotePredictionMapper.kt
+++ app/src/main/java/com/madonnaapps/buswatch/remote/mapper/RemotePredictionMapper.kt
@@ -8,10 +8,12 @@ import javax.inject.Inject
 class RemotePredictionMapper @Inject constructor() : RemoteMapper<RemotePrediction, Prediction?> {
 
     override fun mapFromRemoteObject(remoteObject: RemotePrediction): Prediction? {
+        val vehicleId = remoteObject.vehicleId ?: return null
         val arrivalTime = remoteObject.arrivalTime ?: return null
         val route = remoteObject.route ?: return null
         val destination = remoteObject.destination ?: return null
+        val direction = remoteObject.direction ?: return null
 
-        return Prediction(Date(arrivalTime), route, destination)
+        return Prediction(vehicleId, Date(arrivalTime), route, destination, direction)
     }
 }
