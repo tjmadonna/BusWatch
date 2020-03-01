28b279df800af155c3514f452f20bfea239601f6
Tyler Madonna <developer@madonnaapps.com>
2020-03-01 17:15:01 -0500

    Added vehicleId, destination, and direction properties to Prediction domain models

--- app/src/main/java/com/madonnaapps/buswatch/domain/model/Prediction.kt
+++ app/src/main/java/com/madonnaapps/buswatch/domain/model/Prediction.kt
@@ -3,7 +3,9 @@ package com.madonnaapps.buswatch.domain.model
 import java.util.*
 
 data class Prediction(
+    val vehicleId: String,
     val arrivalTime: Date,
     val route: String,
-    val destination: String
+    val destination: String,
+    val direction: String
 )
