0db317246dac26953ee63cd555799edeaa203544
Tyler Madonna <developer@madonnaapps.com>
2020-03-01 16:16:50 -0500

    Added get predictions url path to TrueTimeApiService

--- app/src/main/java/com/madonnaapps/buswatch/remote/service/TrueTimeApiService.kt
+++ app/src/main/java/com/madonnaapps/buswatch/remote/service/TrueTimeApiService.kt
@@ -1,11 +1,14 @@
 package com.madonnaapps.buswatch.remote.service
 
+import com.madonnaapps.buswatch.BuildConfig
 import com.madonnaapps.buswatch.remote.model.prediction.RemotePredictionResponse
 import io.reactivex.Single
+import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface TrueTimeApiService {
 
+    @GET(BuildConfig.TRUE_TIME_API_GET_PREDICTIONS_PATH)
     fun getPredictions(@Query("stpid") stopCode: String): Single<RemotePredictionResponse>
 
 }
