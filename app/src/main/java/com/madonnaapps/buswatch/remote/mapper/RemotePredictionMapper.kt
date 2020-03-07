2f32a1ae8655a93211561937a505595b4420360a
Tyler Madonna <developer@madonnaapps.com>
2020-03-07 14:12:36 -0500

    Added simple date format to RemotePredictionMapper to parse arrival time

--- app/src/main/java/com/madonnaapps/buswatch/remote/mapper/RemotePredictionMapper.kt
+++ app/src/main/java/com/madonnaapps/buswatch/remote/mapper/RemotePredictionMapper.kt
@@ -2,18 +2,21 @@ package com.madonnaapps.buswatch.remote.mapper
 
 import com.madonnaapps.buswatch.domain.model.Prediction
 import com.madonnaapps.buswatch.remote.model.prediction.RemotePrediction
-import java.util.*
+import java.text.SimpleDateFormat
 import javax.inject.Inject
 
-class RemotePredictionMapper @Inject constructor() : RemoteMapper<RemotePrediction, Prediction?> {
+class RemotePredictionMapper @Inject constructor(
+    private val dateFormat: SimpleDateFormat
+) : RemoteMapper<RemotePrediction, Prediction?> {
 
     override fun mapFromRemoteObject(remoteObject: RemotePrediction): Prediction? {
         val vehicleId = remoteObject.vehicleId ?: return null
         val arrivalTime = remoteObject.arrivalTime ?: return null
+        val arrivalDate = dateFormat.parse(arrivalTime) ?: return null
         val route = remoteObject.route ?: return null
         val destination = remoteObject.destination ?: return null
         val direction = remoteObject.direction ?: return null
 
-        return Prediction(vehicleId, Date(arrivalTime), route, destination, direction)
+        return Prediction(vehicleId, arrivalDate, route, destination, direction)
     }
 }
