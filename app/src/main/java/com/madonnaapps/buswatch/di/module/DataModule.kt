88ec1740fb24239892f3c991a65daa3e02d6050d
Tyler Madonna <developer@madonnaapps.com>
2020-03-07 14:14:38 -0500

    Added bindPredictionRepository to data module

--- app/src/main/java/com/madonnaapps/buswatch/di/module/DataModule.kt
+++ app/src/main/java/com/madonnaapps/buswatch/di/module/DataModule.kt
@@ -1,7 +1,9 @@
 package com.madonnaapps.buswatch.di.module
 
+import com.madonnaapps.buswatch.data.repository.PredictionRepositoryImpl
 import com.madonnaapps.buswatch.data.repository.StopRepositoryImpl
 import com.madonnaapps.buswatch.domain.repository.LocationRepository
+import com.madonnaapps.buswatch.domain.repository.PredictionRepository
 import com.madonnaapps.buswatch.domain.repository.StopRepository
 import com.madonnaapps.buswatch.local.datastore.LocationLocalDataStoreImpl
 import dagger.Binds
@@ -19,4 +21,9 @@ abstract class DataModule {
     abstract fun bindStopRepository(
         repository: StopRepositoryImpl
     ): StopRepository
+
+    @Binds
+    abstract fun bindPredictionRepository(
+        repository: PredictionRepositoryImpl
+    ): PredictionRepository
 }
